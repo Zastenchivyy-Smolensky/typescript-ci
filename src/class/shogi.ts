34553c62387cdd3cc9c2549a9846a3e04f54ea6f
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-03-06 00:45:50 +0900

    finish shogi

---
+++ src/class/shogi.ts
@@ -0,0 +1,72 @@
+type Suji =1|2|3|4|5|6|7|8|9
+type Dan ="1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"
+type Player = "first" | "second"
+class Position {
+    constructor(private suji:Suji, private  dan:Dan){
+
+    }
+    distanceFrom(position: Position, player: Player){
+        if(player ==="first"){
+            return {
+                suji:Math.abs(position.suji - this.suji),
+                dan: Math.abs(Number(position.dan) - Number(this.suji)),
+            }
+        }else{
+            return {
+                suji:Math.abs(position.suji - this.suji),
+                dan: -(Math.abs(Number(position.dan) - Number(this.suji))),
+            }
+        }
+    }
+}
+
+abstract class Piece{
+    protected position:Position
+
+    constructor(
+        private readonly player: Player,
+        suji: Suji,
+        dan: Dan
+    ){
+        this.position = new Position(suji,dan)
+    }
+
+    moveTo(position: Position) {
+        this.position = position
+    }
+
+    abstract canMoveTo(position: Position, player: Player): boolean
+}
+
+class Osho extends Piece{
+    canMoveTo(position: Position, player: Player): boolean {
+        const distance = this.position.distanceFrom(position, player)
+        return (distance.suji < 2 && distance.dan< 2)
+    }
+}
+
+class Game{
+    private piece = Game.makePieces()
+    private static makePieces(){
+        return[
+            new Osho("first",5,"1"),
+            new Osho("second",5,"9")
+
+        ]
+    }
+}
+
+class Fu extends Piece{
+    canMoveTo(position: Position, player: Player): boolean {
+        const distance = this.position.distanceFrom(position,player)
+
+        return distance.suji === 0 && distance.dan === 1
+    }
+}
+class Narikin extends Fu{
+    canMoveTo(position: Position, player: Player): boolean {
+        const distance = this.position.distanceFrom(position, player)
+        return distance.suji <2 && distance.dan <2
+        && (distance.suji !== 0 && distance.dan === -1)
+    }
+}
